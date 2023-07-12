3ffe883c1ffcdb7c7e86aaaeb9cfb08c4f569468
riyadinesh <pdineshselvam@gmail.com>
2023-07-12 18:14:13 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, useEffect, useState } from "react";
+import "./App.css";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 function App() {
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+    console.log("fetchData", fetchData());
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch("https://fakestoreapi.com/products");
+    const jsonData = await data.json();
+    setPost(jsonData);
+
+    console.log("data.json", setPost);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      {post ? (
+        <div>
+          {post.map((post, index) => {
+            return (
+              <div key={index}>
+                <div className="container1">
+                  <div className="imgBox">
+                    <div>
+                      <LazyLoadImage className="img" src={post.image} alt="noImage" />
+                    </div>
+                  </div>
+                  <div className="text">
+                    <h3>{post.title}</h3>
+
+                    <h6>
+                      <div className="rate">
+                        <span>{post.rating.rate}*</span> {post.rating.count}{" "}
+                        Ratings.
+                      </div>
+                    </h6>
+                    <div className="price">₹{Math.round(post.price * 10)}</div>
+
+                    {/* <div>
+                      <p className="category">description : </p>
+                      <p>{post.description}</p>
+                    </div> */}
+
+                    <div><p className="category">category : </p><h5>{post.category}</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <h3>loading...</h3>
+      )}
     </div>
   );
 }
-
 export default App;
